3f1db94c573594dc1a93f4c374e9e1e4640f590d
Jean M Cetoute <jeanmarylerate51@gmail.com>
2019-09-08 13:45:59 -0500

    working with the fetchusers

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './components/App';
-
 import reducers from './reducers/index';
 
 const store = createStore(reducers, applyMiddleware(thunk));
